7343c83f1a0aa2346595577704e6786b79e0d15e
Eric Perlman <perlman@gmail.com>
2026-02-23 00:39:46 -0800

    Use proper fstrings

--- materializationengine/config.py
+++ materializationengine/config.py
@@ -146,7 +146,7 @@ class DevConfig(BaseConfig):
     REDIS_HOST = os.environ.get("REDIS_HOST")
     REDIS_PORT = os.environ.get("REDIS_PORT")
     REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
-    REDIS_URL = f"redis://:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}/0"
+    REDIS_URL = f"redis://{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0"
     CELERY_BROKER_URL = REDIS_URL
     CELERY_RESULT_BACKEND = REDIS_URL
     USE_SENTINEL = os.environ.get("USE_SENTINEL", False)
